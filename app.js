78455585a0a43f25a25de48bb957ff1130179fa5
romannjoroge <roman.njoroge@njuguna.com>
2023-03-22 19:25:30 +0300

    feat: Validating password and email

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const port = 5000;
 app.use(express.json());
 app.use('/auth', auth);
 
+app.get('/test', (req, res) => {
+    res.send("Server can be reached")
+})
+
 app.listen(port, () => {
     console.log(`Server has started at port ${port}...`);
 })
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-    const {username, password} = req.body;
+router.post('/signup', 
+            check('email', "Enter a Valid Email").isEmail(), 
+            check('password', "Password Should Be Longer Than 8 Characters").isLength({min: 8}),
+            (req, res) => {
+    // Check if username and password are valid
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(404).json({erros: errors.array()});
+    }
+    const {username, password, email} = req.body;
     console.log(username, password ,email);
     res.send("POST Request Reached");
 });
